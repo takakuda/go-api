2493e350286b1b793ff116ebb98710f3ff699ac9
kei <kei.takakuda.0415@icloud.com>
2019-12-13 05:22:48 +0900

    feat: use handlers package

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/takakuda/go-api/handlers"
 )
 
 func main() {
 	log.Print("Starting the service...")
-
-	http.HandleFunc("/home", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w, "Hello! Your request was processed.")
-	},
-	)
-
+	router := handlers.Router()
 	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
