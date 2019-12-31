e4e2db665885c9624db2eec035fa61280ab1e4b3
kei <kei.takakuda.0415@icloud.com>
2019-12-31 19:00:21 +0900

    feat: make use of environment variables

--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/takakuda/go-api/handlers"
 )
 
 func main() {
 	log.Print("Starting the service...")
-	router := handlers.Router()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not set.")
+	}
+
+	r := handlers.Router()
 	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
