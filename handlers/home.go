90d627342714b8be63b48ecc1659bc60e531c539
kei <kei.takakuda.0415@icloud.com>
2019-12-31 19:52:40 +0900

    feat: modified handlers/home.go

--- handlers/home.go
+++ handlers/home.go
@@ -1,10 +1,28 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/takakuda/go-api/version"
 )
 
 func home(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, "Hello! Your request was processed.")
+	info := struct {
+		BuildTime string `json:"buildTime"`
+		Commit    string `json:"commit"`
+		Release   string `json:"release"`
+	}{
+		version.BuildTime, version.Commit, version.Release,
+	}
+
+	body, err := json.Marshal(info)
+	if err != nil {
+		log.Printf("Could not encode info data: %v", err)
+		http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body)
 }
