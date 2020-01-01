fdc1c62031b9c078bfac8697496d4b591cdbe4e4
kei <kei.takakuda.0415@icloud.com>
2020-01-01 16:45:07 +0900

    feat: add healthz readyz handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,11 +1,26 @@
 package handlers
 
 import (
+	"log"
+	"sync/atomic"
+	"time"
+
 	"github.com/gorilla/mux"
 )
 
-func Router() *mux.Router {
+func Router(buildTime, commit, release string) *mux.Router {
+	isReady := &atomic.Value{}
+	isReady.Store(true)
+	go func() {
+		log.Printf("Readyz prode is negative by default...")
+		time.Sleep(10 * time.Second)
+		isReady.Store(true)
+		log.Printf("Readyz prode is positive.")
+	}()
+
 	r := mux.NewRouter()
-	r.HandleFunc("/home", home).Methods("GET")
+	r.HandleFunc("/home", home(buildTime, commit, relese)).Methods("GET")
+	r.HandleFunc("/healthz", healthz)
+	r.HandleFunc("/readyz", readyz(isReady))
 	return r
 }
